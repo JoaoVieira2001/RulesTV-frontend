a16e30834ccadef51929426f0d413e93e6db97b9
JoaoVieira2001 <joaoedrvieira01@gmail.com>
2025-03-19 20:32:47 +0000

    Finishing of the Page Browse Default Layout

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -19,29 +19,43 @@ export class BrowseComponent implements OnInit{
     1: {
       title: 'Star Wars: Tales of the Jedi',
       image: 'assets/images/BrowseImages/talesOfTheJedi.png',
-      description: 'A profound explorations of characters like Ahsoka Tano and Count Dooku, set across two entirely new and captivating universes.',
+      details: 'A 6 episode event featuring parables built around Jedi from the prequel era. Journey into the lives' +
+        'of two distincly different Jedi -- Ashoka Tano and Count Dooku. Each will be put to the test as they make choices ' +
+        'that will define their destinies.' + '\n' + 'Some flashing lights sequences or patterns may affect photosensitive viewers.' ,
+      description: 'A child goes throught a rite of passage with unexpected results',
+      releaseDate: '2022',
+      genre: 'Action, Anthology, Animation, Science Fiction',
+      rating: '6+',
+      creator: 'Dave Filoni',
+      starring: 'Ashley Eckstein, Corey Burton',
       episodes: [
-        { title: '1. Life and Death',
+        {
+          title: '1. Life and Death',
           image: 'assets/images/Series_Episodes/TalesOfTheJedi/Episode1.png',
           description: 'Captain Rex and the Bad Batch must infiltrate an enemy base on Anaxes.'
         },
-        { title: 'The Bad Batch',
+        {
+          title: 'The Bad Batch',
           image: 'assets/images/Series_Episodes/TalesOfTheJedi/Episode2.png',
           description: 'Captain Rex and the Bad Batch must infiltrate an enemy base on Anaxes.'
         },
-        { title: 'The Bad Batch',
+        {
+          title: 'The Bad Batch',
           image: 'assets/images/Series_Episodes/TalesOfTheJedi/Episode3.png',
           description: 'Captain Rex and the Bad Batch must infiltrate an enemy base on Anaxes.'
         },
-        { title: 'The Bad Batch',
+        {
+          title: 'The Bad Batch',
           image: 'assets/images/Series_Episodes/TalesOfTheJedi/Episode4.png',
           description: 'Captain Rex and the Bad Batch must infiltrate an enemy base on Anaxes.'
         },
-        { title: 'The Bad Batch',
+        {
+          title: 'The Bad Batch',
           image: 'assets/images/Series_Episodes/TalesOfTheJedi/Episode5.png',
           description: 'Captain Rex and the Bad Batch must infiltrate an enemy base on Anaxes.'
         },
-        { title: 'The Bad Batch',
+        {
+          title: 'The Bad Batch',
           image: 'assets/images/Series_Episodes/TalesOfTheJedi/Episode6.png',
           description: 'Captain Rex and the Bad Batch must infiltrate an enemy base on Anaxes.'
         },
@@ -87,11 +101,32 @@ export class BrowseComponent implements OnInit{
     },
   }
 
-  recommendedShows = [
-    { title: 'Andor', image: 'assets/images/BrowseImages/Andor.png' },
-    { title: 'The Mandalorian', image: 'assets/images/BrowseImages/Mandalorian.png' },
-    { title: 'Ahsoka', image: 'assets/images/BrowseImages/Ahsoka.png' }
-  ];
+  recommendedSeries = [
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/TheRevenant.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/WinterSoldier.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/Moana2.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/LifeOfPi.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/Moana.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/Inside_Out2.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/ELtonJohn.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/Deadpool_Wolverine.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/alien_romulus.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/another_you.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/BansheesOfInisherin.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/Black_Swan.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/BohemianRhapsody.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/CHrisDiStefano.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/drop_dead_years.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/Elemental.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/KindOfKindness.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/lilo_stitch.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/NIghtBitch.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/planet__of_the_apes.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/SUgarCane.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/TheLastKingOfScotland.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/movies/ThreeBillboards.png' },
+    { title: 'Thousand_Blades', image: 'assets/images/DisneyPlus/Paradise.png' },
+  ]
 
   selectTab(tab: string){
     this.selectedTab = tab;
